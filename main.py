5244528f8ddca7e507cf4b8730a86ffbb546ddd3
NekoSarada1101 <ryoyukikazu11@gmail.com>
2022-02-08 02:59:37 +0900

    加工したデータをBQにロードする Fixes #3

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import json
+
 import pandas as pd
-from google.cloud import storage
+from google.cloud import bigquery, storage
 from google.oauth2 import service_account
 
+gsc_service_account_info = json.load(open('cloud_storage_credentials.json'))
+gcs_credentials = service_account.Credentials.from_service_account_info(gsc_service_account_info)
+gcs_client = storage.Client(
+    credentials=gcs_credentials,
+    project=gcs_credentials.project_id,
+)
 
-service_account_info = json.load(open('cloud_storage_credentials.json'))
-credentials = service_account.Credentials.from_service_account_info(service_account_info)
-client = storage.Client(
-    credentials=credentials,
-    project=credentials.project_id,
+bq_service_account_info = json.load(open('bigquery_credentials.json'))
+bq_credentials = service_account.Credentials.from_service_account_info(bq_service_account_info)
+bq_client = bigquery.Client(
+    credentials=bq_credentials,
+    project=bq_credentials.project_id,
 )
 
 
@@ -21,9 +28,12 @@ def bqloader(event, context):
     print('Created: {}'.format(event['timeCreated']))
     print('Updated: {}'.format(event['updated']))
 
-    bucket = client.get_bucket(event['bucket'])
+    bucket = gcs_client.get_bucket(event['bucket'])
     blob = bucket.blob(event['name'])
     blob.download_to_filename('/tmp/' + event['name'])
 
     df = pd.read_csv('/tmp/' + event['name'], header=0)
     df.drop(columns=['利用者', '新規サイン'], inplace=True)
+
+    table = bq_client.get_table('rakuten_card_detail')
+    bq_client.insert_rows_from_dataframe(table, df)
